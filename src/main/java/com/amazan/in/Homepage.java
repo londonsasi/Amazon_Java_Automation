3ad0aee92c97bad1b5a7e82a667564a73b2e8e54
londonsasi <sasi.satha@gmail.com>
2017-11-13 21:07:51 +0000

    fix the failing tests

--- src/main/java/com/amazan/in/Homepage.java
+++ src/main/java/com/amazan/in/Homepage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.Keys;
 public class Homepage extends DriverManager {
     public void searchForProduct() {
 
-        driver.findElement(By.linkText("twotabsearchtextbox")).sendKeys("java programming");
-        driver.findElement(By.linkText("twotabsearchtextbox")).sendKeys(Keys.ENTER);
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("java programming");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys(Keys.ENTER);
     }
 }
